3add1b6b1655f5cacd23649cb3e7884c4b9cbb6b
Ana Gavrilovic <gavrilovic.rs65@uns.ac.rs>
2020-11-18 16:51:15 +0100

    #menu_bar Izmena klase

--- StudentskaSluzbaFTN/src/view/MenuBar.java
+++ StudentskaSluzbaFTN/src/view/MenuBar.java
@@ -10,6 +10,7 @@ import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
 import javax.swing.border.EtchedBorder;
 
+// klasa pisana po ugledu na klasu MyMenuBar iz materijala sa vezbi
 public class MenuBar extends JMenuBar{
 
 	public MenuBar() {
@@ -20,11 +21,11 @@ public class MenuBar extends JMenuBar{
 		JMenu file = new JMenu("File");
 		file.setMnemonic(KeyEvent.VK_F);
 		
-		JMenuItem newFile = new JMenuItem("New");
+		JMenuItem newFile = new JMenuItem("New		");
 		newFile.setIcon(new ImageIcon("images/add.png"));
 		newFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
 		
-		JMenuItem close = new JMenuItem("Close");
+		JMenuItem close = new JMenuItem("Close		");
 		close.setIcon(new ImageIcon("images/close.png"));
 		close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
 		
@@ -37,11 +38,11 @@ public class MenuBar extends JMenuBar{
 		JMenu edit = new JMenu("Edit");
 		edit.setMnemonic(KeyEvent.VK_E);
 		
-		JMenuItem editFile = new JMenuItem("Edit");
+		JMenuItem editFile = new JMenuItem("Edit		");
 		editFile.setIcon(new ImageIcon("images/edit.png"));
 		editFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
 		
-		JMenuItem delete = new JMenuItem("Delete");
+		JMenuItem delete = new JMenuItem("Delete		");
 		delete.setIcon(new ImageIcon("images/delete.png"));
 		delete.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.CTRL_MASK));
 		
@@ -54,11 +55,11 @@ public class MenuBar extends JMenuBar{
 		JMenu help = new JMenu("Help");
 		help.setMnemonic(KeyEvent.VK_H);
 		
-		JMenuItem helpItem = new JMenuItem("Help");
+		JMenuItem helpItem = new JMenuItem("Help		");
 		helpItem.setIcon(new ImageIcon("images/help.png"));
 		helpItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.CTRL_MASK));
 		
-		JMenuItem about = new JMenuItem("About");
+		JMenuItem about = new JMenuItem("About		");
 		about.setIcon(new ImageIcon("images/info.png"));
 		about.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
 		
