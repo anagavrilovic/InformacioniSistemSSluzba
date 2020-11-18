ec3b366888a2b5fac46a6ed0c8273530b96807c8
marijakljestan <73355880+marijakljestan@users.noreply.github.com>
2020-11-18 15:34:52 +0100

    #glavni_prozor Implementacija forme za glavni prozor

--- StudentskaSluzbaFTN/src/main/Main.java
+++ StudentskaSluzbaFTN/src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import view.GlavniProzor;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		GlavniProzor gp = new GlavniProzor();
+		gp.setVisible(true);
 	}
 
 }
