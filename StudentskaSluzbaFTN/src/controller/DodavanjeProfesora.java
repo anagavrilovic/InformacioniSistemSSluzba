391e2013a7cadaed94454781090a59205539c645
marijakljestan <73355880+marijakljestan@users.noreply.github.com>
2020-12-24 16:52:04 +0100

    #dodavanje_profesora Implementacija dijaloga

--- StudentskaSluzbaFTN/src/controller/DodavanjeProfesora.java
+++ StudentskaSluzbaFTN/src/controller/DodavanjeProfesora.java
@@ -1,6 +1,13 @@
 package controller;
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -13,13 +20,13 @@ public class DodavanjeProfesora {
 	private JTextField jtfIme;
 	private JTextField jtfPrz;
 	private JTextField jtfDatum;
-	private JTextField jtfAdresa;
+	private JTextField jtfAdresaStan;
 	private JTextField jtfBrTel;
 	private JTextField jtfeMail;
-	private JTextField jtfBrInd;
-	private JTextField jtfGodUpisa;
-	private JTextField jtfGodStud;
-	private JTextField jtfFinansiranje;
+	private JTextField jtfAdresaKanc;
+	private JTextField jtfBrLK;
+	private JTextField jtfTitula;
+	private JTextField jtfZvanje;
 	
 	
 	public DodavanjeProfesora() {
@@ -29,22 +36,89 @@ public class DodavanjeProfesora {
 		this.jtfIme = new JTextField();
 		this.jtfPrz = new JTextField();
 		this.jtfDatum = new JTextField();
-		this.jtfAdresa= new JTextField();
+		this.jtfAdresaStan= new JTextField();
 		this.jtfBrTel = new JTextField();
 		this.jtfeMail = new JTextField();
-		this.jtfBrInd = new JTextField();
-		this.jtfGodUpisa = new JTextField();
-		this.jtfGodStud = new JTextField();
-		this.jtfFinansiranje = new JTextField();
+		this.jtfAdresaKanc = new JTextField();
+		this.jtfBrLK = new JTextField();
+		this.jtfTitula = new JTextField();
+		this.jtfZvanje = new JTextField();
 	}
 	
 	public DodavanjeProfesora(GlavniProzor gp) {
 		super();
-		this.jd = new JDialog(gp, "Dodavanje profesora", true);
-		this.jp = new JPanel();
-		jd.setSize(400,400);
+		jp = new JPanel();
+	
+		
+		jd.setDefaultLookAndFeelDecorated(true);
+		jd = new JDialog(gp, "Dodavanje profesora", true);
+		jd.setSize(500,600);
 		jd.setResizable(false);
-		jd.setLocationRelativeTo(gp);
+		jd.setLocationRelativeTo(null);
+		
+		
+		jp.setVisible(true);
+		jp.setBackground(Color.WHITE);
+		jp.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+		jp.setLayout(new GridBagLayout());
+
+		JLabel jlIme = new JLabel("Ime*");
+		GridBagConstraints gbcLabIme = new GridBagConstraints();
+		gbcLabIme.gridx = 0;
+		gbcLabIme.gridy = 0;
+		gbcLabIme.gridwidth = 5;
+		gbcLabIme.insets = new Insets(20, 20, 0, 0);
+		gbcLabIme.anchor = GridBagConstraints.LINE_START;
+		jp.add(jlIme, gbcLabIme);
+		
+		jtfIme = new JTextField(20); 
+		GridBagConstraints gbctfIme = new GridBagConstraints();
+		gbctfIme.gridx = 5;
+		gbctfIme.gridy = 0;
+		gbctfIme.weightx = 100;
+		gbctfIme.fill = GridBagConstraints.HORIZONTAL;
+		gbctfIme.insets = new Insets(20, 20, 0, 20);
+		jp.add(jtfIme, gbctfIme);
+		
+		JLabel jlPrz = new JLabel("Prezime*");
+		GridBagConstraints gbcLabPrz = new GridBagConstraints();
+		gbcLabPrz.gridx = 0;
+		gbcLabPrz.gridy = 1;
+		gbcLabPrz.gridwidth = 5;
+		gbcLabPrz.insets = new Insets(20, 20, 0, 0);
+		gbcLabPrz.anchor = GridBagConstraints.LINE_START;
+		jp.add(jlPrz, gbcLabPrz);
+		
+		jtfPrz = new JTextField(20); 
+		GridBagConstraints gbctfPrz = new GridBagConstraints();
+		gbctfPrz.gridx = 5;
+		gbctfPrz.gridy = 1;
+		gbctfPrz.weightx = 100;
+		gbctfPrz.fill = GridBagConstraints.HORIZONTAL;
+		gbctfPrz.insets = new Insets(20, 20, 0, 20);
+		jp.add(jtfPrz, gbctfPrz);
+		
+		JLabel jlDatum = new JLabel("Datum rođenja*");
+		GridBagConstraints gbcLabDatum = new GridBagConstraints();
+		gbcLabDatum.gridx = 0;
+		gbcLabDatum.gridy = 2;
+		gbcLabDatum.gridwidth = 5;
+		gbcLabDatum.insets = new Insets(20, 20, 0, 0);
+		gbcLabDatum.anchor = GridBagConstraints.LINE_START;
+		jp.add(jlDatum, gbcLabDatum);
+		
+		jtfDatum = new JTextField(20); 
+		GridBagConstraints gbctfDatum = new GridBagConstraints();
+		gbctfDatum.gridx = 5;
+		gbctfDatum.gridy = 2;
+		gbctfDatum.weightx = 100;
+		gbctfDatum.fill = GridBagConstraints.HORIZONTAL;
+		gbctfDatum.insets = new Insets(20, 20, 0, 20);
+		jp.add(jtfDatum, gbctfDatum);
+		
+		jd.add(jp);
+		jd.setVisible(true);
+
 	}
 	
 }
