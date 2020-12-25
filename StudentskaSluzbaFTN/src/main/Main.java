cd580f1311a73ddec26908ad6a1c810e60c773c0
Ana Gavrilovic <gavrilovic.rs65@uns.ac.rs>
2020-12-25 14:39:54 +0100

    Izmena u klasi StudentTable

--- StudentskaSluzbaFTN/src/main/Main.java
+++ StudentskaSluzbaFTN/src/main/Main.java
@@ -12,7 +12,6 @@ import view.GlavniProzor;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		GlavniProzor gp = GlavniProzor.getInstance();
 		gp.setIconImage(new ImageIcon("images/user_group.png").getImage());
 		gp.setVisible(true);
