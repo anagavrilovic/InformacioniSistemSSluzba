6fa4eedb24e0f030defa0164911a4539e87f04ad
marijakljestan <73355880+marijakljestan@users.noreply.github.com>
2020-12-28 15:24:21 +0100

    #brisanje_predmeta Implementacija funkcionalnosti

--- StudentskaSluzbaFTN/src/view/CreateDocumentActionDelete.java
+++ StudentskaSluzbaFTN/src/view/CreateDocumentActionDelete.java
@@ -7,7 +7,12 @@ import java.awt.event.KeyEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
+import javax.swing.WindowConstants;
+
+import controller.PredmetController;
 
 public class CreateDocumentActionDelete extends AbstractAction {
 
@@ -24,6 +29,39 @@ public class CreateDocumentActionDelete extends AbstractAction {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		
+		if(TabbedPane.getInstance().getIndex() == 0) {
+			
+		} else if(TabbedPane.getInstance().getIndex() == 1) {
+			
+		} else if(TabbedPane.getInstance().getIndex() == 2) {
+			
+			JFrame frame = GlavniProzor.getInstance();
+			
+			String[] options = {"Da", "Ne" };
+			
+			if(TabbedPane.getInstance().getSifraPredFromSelectedRow().equals("")) {
+				JOptionPane.showMessageDialog(GlavniProzor.getInstance(), "Morate selektovati red!", 
+											  "Nije selektovan nijedan red", JOptionPane.INFORMATION_MESSAGE, 
+											  GlavniProzor.resizeIcon(new ImageIcon("images/minus.png")));
+				return;
+			}
+			
+			int code = JOptionPane.showOptionDialog(frame.getContentPane(),"Da li ste sigurni da želite da izbrišete selektovani predmet?",
+					"Brisanje predmeta", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
+					 GlavniProzor.resizeIcon(new ImageIcon("images/question.png")), options, null);
+			
+			if (code == JOptionPane.YES_OPTION) {
+				String ret = PredmetController.getInstance().izbrisiPredmet(TabbedPane.getInstance().getSifraPredFromSelectedRow());
+				JOptionPane.showMessageDialog(frame, ret, "Brisanje predmeta", JOptionPane.INFORMATION_MESSAGE,
+											   GlavniProzor.resizeIcon(new ImageIcon("images/trash.png")));
+				
+			} else {
+				frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			}
+			
+			
+			
+		}
 	}
 
 }
